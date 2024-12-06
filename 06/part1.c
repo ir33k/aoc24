0f8cdff9f2e41af1c3d853dba60dad95b44bff9a
irek <irek@gabr.pl>
2024-12-06 14:25:52 +0100

    refactor

--- 06/part1.c
+++ 06/part1.c
@@ -1,50 +1,38 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-enum {UP=0, RIGHT, DOWN, LEFT, _DIR_COUNT};
+enum {UP, RIGHT, DOWN, LEFT, _COUNT};	// Directions
 
-int
-main(void)
-{
-	int i, dir;
+int main(void) {
 	unsigned result;
-	char map[256][256];
+	char map[256][256];	// Map with [y][x] coordinates
 	int w,h;	// Map size
-	int x,y;	// Guard position
-	for (i=0; fgets(map[i], sizeof map[i], stdin); i++);
-	h = i;
-	w = strlen(map[0]) - 1;
-	// Find guard starting position.
+	int x,y, dx,dy, dir;	// Guard current and delta position and walking direction
+	// Get map with height and width
+	for (h=0; fgets(map[h], sizeof map[0], stdin); h++);
+	w = strlen(map[0]) -1;	// -1 for '\n' character at the end of the line
 	for (y=0; y<h; y++)
 	for (x=0; x<w; x++) {
 		if (map[y][x] == '^') {
-			goto found;
+			goto found;	// Found guard starting position
 		}
 	}
-found:
-	// Walk the guard.
+found:	// Walk the guard.
 	result = 0;
-	dir = UP;
+	dir = UP;	// Initial direction is always UP
 	while (x<w && x>=0 && y<h && y>=0) {
-		if (map[y][x] != 'X') {
-			result++;
-			map[y][x] = 'X';
-		}
+		result += map[y][x] != 'X';
+		map[y][x] = 'X';
 		switch (dir) {
-		case UP:    y--; break;
-		case RIGHT: x++; break;
-		case DOWN:  y++; break;
-		case LEFT:  x--; break;
+		case UP:    dx= 0; dy=-1; break;
+		case RIGHT: dx=+1; dy= 0; break;
+		case DOWN:  dx= 0; dy=+1; break;
+		case LEFT:  dx=-1; dy= 0; break;
 		}
+		x+=dx; y+=dy;	// Apply delta position
 		if (map[y][x] == '#') {
-			switch (dir) {
-			case UP:    y++; break;
-			case RIGHT: x--; break;
-			case DOWN:  y--; break;
-			case LEFT:  x++; break;
-			}
-			dir = (dir + 1) % _DIR_COUNT;	// Turn right
+			x-=dx; y-=dy;	// Go back one step
+			dir = (dir+1) % _COUNT;	// Turn right by switching to next direction
 		}
 	}
 	printf("%u\n", result);
