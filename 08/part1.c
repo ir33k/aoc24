0fd51a712949472043de134d6437148c284f1d35
irek <irek@gabr.pl>
2024-12-08 14:45:50 +0100

    refactor

--- 08/part1.c
+++ 08/part1.c
@@ -2,69 +2,59 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct antena_t {
-	struct antena_t *next;
+static char map[256][256];	// Map with [y][x] coordinates
+static int w,h;			// Map size
+
+struct antena {
+	struct antena *next;	// Linked list, an elegant weapon
 	int x,y;
 };
 
-int
-main(void)
+static int
+add_antinode(struct antena *a, struct antena *b)
 {
-	int i,c;
-	char map[256][256];	// Map with [y][x] coordinates
-	int w,h;	// Map size
-	int antena_map[128]={0};	// ASCII indexed map to antenas indexes
-	struct antena_t *new, *node, *pair, *antenas[256]={0};
-	int x,y;
-	unsigned result;
-	result = 0;
+	int x = a->x + a->x - b->x;
+	int y = a->y + a->y - b->y;
+	if (x>=0 && x<w && y>=0 && y<h && map[y][x]) {
+		map[y][x] = 0;
+		return 1;
+	}
+	return 0;
+}
+
+int main(void) {
+	int x,y, c, result;
+	struct antena *node, *child, *antenas[128]={0};
 	// Get map with height and width
 	for (h=0; fgets(map[h], sizeof map[0], stdin); h++);
 	w = strlen(map[0]) -1;	// -1 for '\n' character at the end of the line
 	// Gather antenas
-	i=1;
 	for (y=0; y<h; y++)
 	for (x=0; x<w; x++) {
 		c = map[y][x];
-		if (c != '.') {
-			if (!antena_map[c]) {
-				antena_map[c] = i++;
-			}
-			new = malloc(sizeof *new);
-			new->next = 0;
-			new->x = x;
-			new->y = y;
-			node = antenas[antena_map[c]];
-			if (!node) {
-				antenas[antena_map[c]] = new;
-			} else {
-				for (; node->next; node = node->next);
-				node->next = new;
-			}
-		}
-	}
-	// Go over each antena type
-	for (c=0; c < 128; c++) {
-		if (!antena_map[c]) {
+		if (c == '.') {
 			continue;
 		}
-		node = antenas[antena_map[c]];
-		for (; node->next; node = node->next)
-		for (pair = node->next; pair; pair = pair->next) {
-			x = node->x + (node->x - pair->x);
-			y = node->y + (node->y - pair->y);
-			if (x>=0 && x<w && y>=0 && y<h && map[y][x] != '#') {
-				map[y][x] = '#';
-				result++;
-			}
-			x = pair->x + (pair->x - node->x);
-			y = pair->y + (pair->y - node->y);
-			if (x>=0 && x<w && y>=0 && y<h && map[y][x] != '#') {
-				map[y][x] = '#';
-				result++;
-			}
+		child = malloc(sizeof *child);	// Let it leak
+		child->next = 0;
+		child->x = x;
+		child->y = y;
+		if (antenas[c]) {
+			for (node = antenas[c]; node->next; node = node->next);
+			node->next = child;
+		} else {
+			antenas[c] = child;
 		}
 	}
-	printf("%u\n", result);
+	// Place antinodes for each antena type pair and sum the result
+	result = 0;
+	for (c=0; c<128; c++) {
+		for (node = antenas[c]; node && node->next; node = node->next)
+		for (child = node->next; child; child = child->next) {
+			result += add_antinode(node, child);
+			result += add_antinode(child, node);
+		}
+	}
+	printf("%d\n", result);
 	return 0;
 }
--- 08/part2.c
+++ 08/part2.c
@@ -2,87 +2,68 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct antena_t {
-	struct antena_t *next;
+static char map[256][256];	// Map with [y][x] coordinates
+static int w,h;			// Map size
+
+struct antena {
+	struct antena *next;	// Linked list, an elegant weapon
 	int x,y;
 };
+
+static void
+add_antinodes(struct antena *a, struct antena *b)
+{
+	int dx = a->x - b->x;
+	int dy = a->y - b->y;
+	int x = a->x;
+	int y = a->y;
+	map[y][x] = 0;
+	for (x+=dx, y+=dy; x>=0 && x<w && y>=0 && y<h; x+=dx, y+=dy) {
+		map[y][x] = 0;
+	}
+}
 
 int
 main(void)
 {
-	int i,c;
-	char map[256][256];	// Map with [y][x] coordinates
-	int w,h;	// Map size
-	int antena_map[128]={0};	// ASCII indexed map to antenas indexes
-	struct antena_t *new, *node, *pair, *antenas[256]={0};
-	int x,y, dx,dy;
-	unsigned result;
-	result = 0;
+	int x,y, c, result;
+	struct antena *node, *child, *antenas[128]={0};
 	// Get map with height and width
 	for (h=0; fgets(map[h], sizeof map[0], stdin); h++);
 	w = strlen(map[0]) -1;	// -1 for '\n' character at the end of the line
 	// Gather antenas
-	i=1;
 	for (y=0; y<h; y++)
 	for (x=0; x<w; x++) {
 		c = map[y][x];
-		if (c != '.') {
-			if (!antena_map[c]) {
-				antena_map[c] = i++;
-			}
-			new = malloc(sizeof *new);
-			new->next = 0;
-			new->x = x;
-			new->y = y;
-			node = antenas[antena_map[c]];
-			if (!node) {
-				antenas[antena_map[c]] = new;
-			} else {
-				for (; node->next; node = node->next);
-				node->next = new;
-			}
-		}
-	}
-	// Go over each antena type
-	for (c=0; c < 128; c++) {
-		if (!antena_map[c]) {
+		if (c == '.') {
 			continue;
 		}
-		node = antenas[antena_map[c]];
-		for (; node->next; node = node->next) {
-			for (pair = node->next; pair; pair = pair->next) {
-				dx = node->x - pair->x;
-				dy = node->y - pair->y;
-				x = node->x;
-				y = node->y;
-				map[y][x] = '#';
-				x += dx;
-				y += dy;
-				while (x>=0 && x<w && y>=0 && y<h) {
-					map[y][x] = '#';
-					x += dx;
-					y += dy;
-				}
-				dx = pair->x - node->x;
-				dy = pair->y - node->y;
-				x = pair->x;
-				y = pair->y;
-				map[y][x] = '#';
-				x += dx;
-				y += dy;
-				while (x>=0 && x<w && y>=0 && y<h) {
-					map[y][x] = '#';
-					x += dx;
-					y += dy;
-				}
-			}
+		child = malloc(sizeof *child);	// Let it leak
+		child->next = 0;
+		child->x = x;
+		child->y = y;
+		if (antenas[c]) {
+			for (node = antenas[c]; node->next; node = node->next);
+			node->next = child;
+		} else {
+			antenas[c] = child;
+		}
+	}
+	// Place antinodes for each antena type pair
+	result = 0;
+	for (c=0; c<128; c++) {
+		for (node = antenas[c]; node && node->next; node = node->next)
+		for (child = node->next; child; child = child->next) {
+			add_antinodes(node, child);
+			add_antinodes(child, node);
 		}
 	}
 	// Count antiantenas
+	result = 0;
 	for (y=0; y<h; y++)
 	for (x=0; x<w; x++) {
-		result += map[y][x] == '#';
+		result += !map[y][x];
 	}
-	printf("%u\n", result);
+	printf("%d\n", result);
 	return 0;
 }
