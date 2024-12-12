6e8437e3ad9ef03af13a71e182ab42f45b4d436b
irek <irek@gabr.pl>
2024-12-12 08:46:43 +0100

    refactor

--- 12/part1.c
+++ 12/part1.c
@@ -3,27 +3,34 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Use lowercase letters to mark visited plants
 #define TO_UPPER(c)	(0xDF & c)
 #define TO_LOWER(c)	(0x20 | c)
 #define IS_LOWER(c)	(0x20 & c)
 
-struct point { int x,y; };
-
 static char map[256][256];	// Map with [y][x] coordinates
-static int w,h;		// Map size
+static int w,h;			// Map size
 
-static int is_border(int plant, int x, int y)
-{
-	return x<0 || x>=w || y<0 || y>=h || TO_UPPER(map[y][x]) != plant;
+static struct point {
+	int x,y;
+} stack[1024];			// trust me bro, this is enough
+static unsigned si;		// Stack index
+
+static int check(int plant, int x, int y) {
+	if (x<0 || x>=w || y<0 || y>=h || TO_UPPER(map[y][x]) != plant) {
+		return 1;	// Outside of plant border
+	}
+	stack[si].x = x;
+	stack[si].y = y;
+	si++;
+	assert(si < sizeof stack / sizeof stack[0]);
+	return 0;
 }
 
 int main(void) {
 	int x,y;		// Current position
-	int dx,dy;
-	unsigned i, plant, area, perimeter;
+	int plant, area, perimeter;
 	long unsigned result;
-	int border;
-	struct point stack[256];
 	//
 	for (h=0; fgets(map[h], sizeof map[0], stdin); h++);
 	w = strlen(map[0]) -1;	// -1 for '\n' character at the end of the line
@@ -35,54 +42,22 @@ int main(void) {
 		plant = map[y][x];
 		area = 0;
 		perimeter = 0;
-		i = 0;
-		stack[i].x = x;
-		stack[i].y = y;
+		si = 0;
+		stack[si].x = x;
+		stack[si].y = y;
 		do {
-			x = stack[i].x;
-			y = stack[i].y;
+			x = stack[si].x;
+			y = stack[si].y;
 			if (IS_LOWER(map[y][x])) {
 				continue;
 			}
 			area++;
 			map[y][x] = TO_LOWER(plant);	// Mark as visited
-			//
-			dx=x+0; dy=y+1;
-			border = is_border(plant, dx, dy);
-			perimeter += border;
-			if (!border && !IS_LOWER(map[dy][dx])) {
-				stack[i].x = dx;
-				stack[i].y = dy;
-				i++;
-			}
-			//
-			dx=x+0; dy=y-1;
-			border = is_border(plant, dx, dy);
-			perimeter += border;
-			if (!border && !IS_LOWER(map[dy][dx])) {
-				stack[i].x = dx;
-				stack[i].y = dy;
-				i++;
-			}
-			//
-			dx=x+1; dy=y+0;
-			border = is_border(plant, dx, dy);
-			perimeter += border;
-			if (!border && !IS_LOWER(map[dy][dx])) {
-				stack[i].x = dx;
-				stack[i].y = dy;
-				i++;
-			}
-			//
-			dx=x-1; dy=y+0;
-			border = is_border(plant, dx, dy);
-			perimeter += border;
-			if (!border && !IS_LOWER(map[dy][dx])) {
-				stack[i].x = dx;
-				stack[i].y = dy;
-				i++;
-			}
-		} while (i--);
+			perimeter += check(plant, x  , y+1);
+			perimeter += check(plant, x  , y-1);
+			perimeter += check(plant, x+1, y  );
+			perimeter += check(plant, x-1, y  );
+		} while (si--);
 		result += area * perimeter;
 		// Find next plant
 		for (y=0; y<h; y++)
