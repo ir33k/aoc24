d796fcdf279afdb272ebb76bc0e53ffcd5caf924
irek <irek@gabr.pl>
2024-12-14 08:24:39 +0100

    refactor

--- 14/part1.c
+++ 14/part1.c
@@ -1,76 +1,51 @@
-#include <math.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 #define MOD(a,b) (((a) % (b) + (b)) % (b))
 
-struct v2 { int x,y; };
-struct robot { struct v2 p,v; };
+struct robot {
+	int px,py;	// Position
+	int vx,vy;	// Velocity
+};
 
-static int w, h;
 static struct robot robots[512];
 static int rcount=0;
 
+static unsigned count(int from_x, int from_y, int to_x, int to_y) {
+	int x,y,i,n=0;
+	for (y=from_y; y<to_y; y++)
+	for (x=from_x; x<to_x; x++)
+	for (i=0; i<rcount; i++) {
+		n += robots[i].px == x && robots[i].py == y;
+	}
+	return n;
+}
+
 int main(void) {
-	int i,j, x,y, n;
+	int i, w,h;
+	int mx,my;	// Middle line
 	char buf[4096];
-	struct v2 mid;
 	long unsigned result;
 	// NOTE(irek): I added width and height information to input files
 	// on the first line as example and puzzle input have different
 	// width and height which normally had to be hardcoded.
 	fgets(buf, sizeof buf, stdin);
 	sscanf(buf, "w=%d h=%d", &w, &h);
-	mid.x = w/2;
-	mid.y = h/2;
+	mx = w/2;
+	my = h/2;
 	while (fgets(buf, sizeof buf, stdin)) {
 		sscanf(buf, "p=%d,%d v=%d,%d",
-		       &robots[rcount].p.x,
-		       &robots[rcount].p.y,
-		       &robots[rcount].v.x,
-		       &robots[rcount].v.y);
+		       &robots[rcount].px, &robots[rcount].py,
+		       &robots[rcount].vx, &robots[rcount].vy);
 		rcount++;
 	}
-	for (i=0; i<rcount; i++)
-	for (j=0; j<100; j++) {
-		robots[i].p.x = MOD(robots[i].p.x + robots[i].v.x, w);
-		robots[i].p.y = MOD(robots[i].p.y + robots[i].v.y, h);
-	}
-
-	result=1;
-	n=0;
-
-	for (y=0; y<mid.y; y++)
-	for (x=0; x<mid.x; x++)
 	for (i=0; i<rcount; i++) {
-		n += robots[i].p.x == x && robots[i].p.y == y;
+		robots[i].px = MOD(robots[i].px + robots[i].vx*100, w);
+		robots[i].py = MOD(robots[i].py + robots[i].vy*100, h);
 	}
-	result *= n;
-
-	n=0;
-	for (y=0; y<mid.y; y++)
-	for (x=mid.x+1; x<w; x++)
-	for (i=0; i<rcount; i++) {
-		n += robots[i].p.x == x && robots[i].p.y == y;
-	}
-	result *= n;
-
-	n=0;
-	for (y=mid.y+1; y<h; y++)
-	for (x=mid.x+1; x<w; x++)
-	for (i=0; i<rcount; i++) {
-		n += robots[i].p.x == x && robots[i].p.y == y;
-	}
-	result *= n;
-
-	n=0;
-	for (y=mid.y+1; y<h; y++)
-	for (x=0; x<mid.x; x++)
-	for (i=0; i<rcount; i++) {
-		n += robots[i].p.x == x && robots[i].p.y == y;
-	}
-	result *= n;
-
+	result  = count(0,    0,    mx, my);	// Top left
+	result *= count(mx+1, 0,    w,  my);	// Top right
+	result *= count(mx+1, my+1, w,  h);	// Bottom right
+	result *= count(0,    my+1, mx, h);	// Bottom left
 	printf("%lu\n", result);
 	return 0;
 }
