65d668d1643cf2e3e4e91cb41a219517491107cf
irek <irek@gabr.pl>
2024-12-14 10:44:34 +0100

    refactor

--- 14/part1.c
+++ 14/part1.c
@@ -2,50 +2,26 @@
 
 #define MOD(a,b) (((a) % (b) + (b)) % (b))
 
-struct robot {
-	int px,py;	// Position
-	int vx,vy;	// Velocity
-};
-
-static struct robot robots[512];
-static int rcount=0;
-
-static unsigned count(int from_x, int from_y, int to_x, int to_y) {
-	int x,y,i,n=0;
-	for (y=from_y; y<to_y; y++)
-	for (x=from_x; x<to_x; x++)
-	for (i=0; i<rcount; i++) {
-		n += robots[i].px == x && robots[i].py == y;
-	}
-	return n;
-}
-
 int main(void) {
-	int i, w,h;
-	int mx,my;	// Middle line
+	int i;
+	int px,py, vx,vy;	// Robot position and velocity
+	int w,h, mx,my;		// Space size and middle line
+	unsigned square[4]={0};
 	char buf[4096];
-	long unsigned result;
-	// NOTE(irek): I added width and height information to input files
-	// on the first line as example and puzzle input have different
-	// width and height which normally had to be hardcoded.
+	// NOTE(irek): I stored width and height information in input.
 	fgets(buf, sizeof buf, stdin);
 	sscanf(buf, "w=%d h=%d", &w, &h);
 	mx = w/2;
 	my = h/2;
-	while (fgets(buf, sizeof buf, stdin)) {
-		sscanf(buf, "p=%d,%d v=%d,%d",
-		       &robots[rcount].px, &robots[rcount].py,
-		       &robots[rcount].vx, &robots[rcount].vy);
-		rcount++;
+	for (i=0; fgets(buf, sizeof buf, stdin); i++) {
+		sscanf(buf, "p=%d,%d v=%d,%d", &px, &py, &vx, &vy);
+		px = MOD(px + vx*100, w);
+		py = MOD(py + vy*100, h);
+		square[0] += px<mx && py<my;	// Top left
+		square[1] += px>mx && py<my;	// Top right
+		square[2] += px>mx && py>my;	// Bottom right
+		square[3] += px<mx && py>my;	// Bottom left
 	}
-	for (i=0; i<rcount; i++) {
-		robots[i].px = MOD(robots[i].px + robots[i].vx*100, w);
-		robots[i].py = MOD(robots[i].py + robots[i].vy*100, h);
-	}
-	result  = count(0,    0,    mx, my);	// Top left
-	result *= count(mx+1, 0,    w,  my);	// Top right
-	result *= count(mx+1, my+1, w,  h);	// Bottom right
-	result *= count(0,    my+1, mx, h);	// Bottom left
-	printf("%lu\n", result);
+	printf("%u\n", square[0] * square[1] * square[2] * square[3]);
 	return 0;
 }
