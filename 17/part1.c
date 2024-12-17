14463e60506856f480e313ed2fd6e82d7e069e81
irek <irek@gabr.pl>
2024-12-17 08:30:09 +0100

    refactor

--- 17/part1.c
+++ 17/part1.c
@@ -4,39 +4,37 @@
 
 int main(void) {
 	char buf[4096];
-	int i, opcode, operand, combo;
 	int program[32];	// List of instructions
-	int icount=0;		// Instructions count
-	int reg_a=0, reg_b=0, reg_c=0;
-	scanf("Register A: %d\n", &reg_a);
-	scanf("Register B: %d\n", &reg_b);
-	scanf("Register C: %d\n", &reg_c);
+	int i, n=0;		// Index and number of instructions
+	int a=0, b=0, c=0;	// Registers
+	int op, combo;		// Operand and combo value
+	scanf("Register A: %d\n", &a);
+	scanf("Register B: %d\n", &b);
+	scanf("Register C: %d\n", &c);
 	fgets(buf, sizeof buf, stdin);	// Skip line
 	fgets(buf, sizeof buf, stdin);
 	i=0;
 	while (buf[i] != ' ') i++;
-	for (i++; buf[i]; i++) {
-		program[icount++] = atoi(buf+i);
-		while (buf[i] > ',') i++;
+	for (i++; buf[i]; i+=2) {
+		program[n++] = atoi(buf+i);
 	}
-	for (i=0; i<icount; i+=2) {
-		opcode = program[i];
-		operand = program[i+1];
-		combo = operand;
-		switch (operand) {
-		case 4: combo = reg_a; break;
-		case 5: combo = reg_b; break;
-		case 6: combo = reg_c; break;
+	for (i=0; i<n; i+=2) {
+		op = program[i+1];
+		combo = op;
+		switch (op) {
+		case 4: combo = a; break;
+		case 5: combo = b; break;
+		case 6: combo = c; break;
 		}
-		switch (opcode) {
-		case 0: reg_a = reg_a / pow(2, combo); break;
-		case 1: reg_b = reg_b ^ operand;       break;
-		case 2: reg_b = combo % 8;             break;
-		case 3: if (reg_a != 0) i = operand-2; break;
-		case 4: reg_b = reg_b ^ reg_c;         break;
-		case 5: printf("%d,", combo % 8);      break;
-		case 6: reg_b = reg_a / pow(2, combo); break;
-		case 7: reg_c = reg_a / pow(2, combo); break;
+		switch (program[i]) {
+		case 0: a /= pow(2, combo); break;
+		case 1: b ^= op; break;
+		case 2: b  = combo % 8; break;
+		case 3: if (a) i = op-2; break;
+		case 4: b ^= c; break;
+		case 5: printf("%d,", combo % 8); break;
+		case 6: b = a / pow(2, combo); break;
+		case 7: c = a / pow(2, combo); break;
 		}
 	}
 	printf("\b\n");
