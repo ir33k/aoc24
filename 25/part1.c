cba19f06dd5abf68d7a4af29e08bdee6f90c90ad
irek <irek@gabr.pl>
2024-12-25 23:10:15 +0100

    crazzzzzy implementation

--- 25/part1.c
+++ 25/part1.c
@@ -1,28 +1,22 @@
 #include <stdio.h>
-#define SIZE 5	// Key and lock height and width
 
 int main(void) {
-	int locks[512]={0}, li=0;
-	int  keys[512]={0}, ki=0;
-	int i,j, *tmp, result=0;
+	long i,j, list[512]={0}, result=0;
 	char buf[8];
-	do {
-		fgets(buf, sizeof buf, stdin);
-		tmp = buf[0] == '#' ? &locks[li++] : &keys[ki++];
-		for (i=0; i<SIZE; i++) {
-			fgets(buf, sizeof buf, stdin);
-			for (j=0; j<SIZE; j++) {
-				if (buf[j] == '#') {
-					*tmp |= 1 << (i+j*SIZE);
-				}
+	for (i=0; fgets(buf, sizeof buf, stdin); i++) {
+		if (buf[0] == '\n') {
+			continue;
+		}
+		for (j=0; j<5; j++) {
+			if (buf[j] == '#') {
+				list[i/8] |= 1L << ((i%8)+j*7);
 			}
 		}
-		fgets(buf, sizeof buf, stdin);
-	} while (fgets(buf, sizeof buf, stdin));
-	for (i=0; i<li; i++)
-	for (j=0; j<ki; j++) {
-		result += !(locks[i] & keys[j]);
+	};
+	for (i=0;   list[i]; i++)
+	for (j=i+1; list[j]; j++) {
+		result += !(list[i] & list[j]);
 	}
-	printf("%d\n", result);
+	printf("%ld\n", result);
 	return !(result == 3327);
 }
