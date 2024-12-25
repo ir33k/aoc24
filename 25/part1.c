b0ce99c84393481c86b951762744a198499a20b8
irek <irek@gabr.pl>
2024-12-25 22:40:34 +0100

    Work on bit level

--- 25/part1.c
+++ 25/part1.c
@@ -2,29 +2,26 @@
 #define SIZE 5	// Key and lock height and width
 
 int main(void) {
-	int locks[512][SIZE]={0}, li=0;
-	int  keys[512][SIZE]={0}, ki=0;
-	int i,j,k, *array, result=0;
+	int locks[512]={0}, li=0;
+	int  keys[512]={0}, ki=0;
+	int i,j, *tmp, result=0;
 	char buf[8];
 	do {
 		fgets(buf, sizeof buf, stdin);
-		array = buf[0] == '#' ? locks[li++] : keys[ki++];
+		tmp = buf[0] == '#' ? &locks[li++] : &keys[ki++];
 		for (i=0; i<SIZE; i++) {
 			fgets(buf, sizeof buf, stdin);
 			for (j=0; j<SIZE; j++) {
-				array[j] += buf[j] == '#';
+				if (buf[j] == '#') {
+					*tmp |= 1 << (i+j*SIZE);
+				}
 			}
 		}
 		fgets(buf, sizeof buf, stdin);
 	} while (fgets(buf, sizeof buf, stdin));
 	for (i=0; i<li; i++)
 	for (j=0; j<ki; j++) {
-		for (k=0; k<SIZE; k++) {
-			if (locks[i][k] + keys[j][k] > SIZE) {
-				break;
-			}
-		}
-		result += k == SIZE;
+		result += !(locks[i] & keys[j]);
 	}
 	printf("%d\n", result);
 	return !(result == 3327);
